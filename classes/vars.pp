6c2f12801d1db44b15aaaddf3d45fb2e7fc2585d
Ashot Ayvazyan <armstyle@gmail.com>
2014-03-25 12:52:56 -0700

    added comments to config file

--- classes/vars.pp
+++ classes/vars.pp
@@ -1,18 +1,51 @@
+#Specifies Apache package name, which is being provided to package manager such as yum. 
 $apache = "httpd"
+
+#Specifies php package name, which is being provided to package manager such as yum.
 $phpv = "php"
+
+#Specifies mysql-server package name, which is being provided to package manager such as yum.
 $mysqlpkg = "mysql-server"
+
+#Specifies mysql service name to start
 $mysqld = "mysqld"
+
+#Specifies mysql server root password
 $current_mysqlroot_pass =""
+
+#Specifies the method by which will be downloaded wordpress files.
+#Can be set to WEB or GIT. 
+#In case if is set to WEB as $wp_get_address must be provided the web #URL from which the wordpress tarball should be download. 
+#In case if set to GIT, as $wp_get_address must be provided the git #clone url.
 $wrp_metod = "GIT"
 $wp_get_address ="git://core.git.wordpress.org/"
+
 #$wrp_metod = "WEB"
 #$wp_get_address ="https://wordpress.org/latest.tar.gz"
+
+#Specifies the directory into which wordpress should be installed.
 $wp_local_path = "/var/www/html/"
-$mysqlconfigure = "Yes"
-$mysqldinstall = "Yes"
-$wrp_dbname = "wordpress"
-$wrp_dbuser = "wp_user"
-$wrp_dbpass = "secretwppass"
-$wrp_dbhost_access = "localhost"
+
+#Specifies Owner of wordpress install directory
 $wrp_owner = "apache"
+
+#Specifies Group of wordpress install directory
 $wrp_group = "apache"
+
+#Specifies whether to create mysql database, mysql user for wordpress and grant accesses for the created user to wordpress database. 
+$mysqlconfigure = "Yes"
+
+#Specifies whether to install mysql-server.
+$mysqldinstall = "Yes"
+
+#Specifies database name for wordpress
+$wrp_dbname = "wordpress"
+
+#Specifies mysql username for wordpress
+$wrp_dbuser = "wp_user"
+
+#Specifies mysql Password for wordpress
+$wrp_dbpass = "secretwppass"
+
+#The host from which will be granted access for wordpress user to wordpress database
+$wrp_dbhost_access = "localhost"
