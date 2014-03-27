43ee8e919b24d893855ebfa1bb945a71decdcde5
Risk <risk@localhost.localdomain>
2014-03-27 16:06:18 +0800

    Add is root check on install.sh
    So the user can't run install.sh if he isn't using sudo
    or logged in as root

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 #This script is intended for automatic wordpress multisite installation on Centos 6.5 x86_64 minimal installation machines 
 
 file=classes/vars.pp
