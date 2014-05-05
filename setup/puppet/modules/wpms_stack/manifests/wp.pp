725c75514c0d7b9d0d9f25fe234dc1403b695118
KarelOK <karel@link7.co>
2014-05-05 19:26:01 +0800

    move communication file between wp & scripts to other directory
    related to ticket #4

--- setup/puppet/modules/wpms_stack/manifests/wp.pp
+++ setup/puppet/modules/wpms_stack/manifests/wp.pp
@@ -1,16 +1,22 @@
 define wpms_stack::wp() {
 	file { "/var/wpms-stack/www/wordpress/wp-content/mu-plugins/wpms-stack-helper.php":
 		source => "puppet:///modules/wpms_stack/wp/wpms-stack-helper/wpms-stack-helper.php",
-		require => File['/var/wpms-stack/www/wordpress/wp-content/mu-plugins','/var/wpms-stack/www/wpms-stack-plugin-instructions-for-puppet'],
+		require => File['/var/wpms-stack/www/wordpress/wp-content/mu-plugins','/var/wpms-stack/internal/wpms-stack-plugin-instructions-for-puppet'],
 	}
 
-	file { "/var/wpms-stack/www/wordpress/wp-content/mu-plugins":
+	file { ["/var/wpms-stack/www/wordpress/wp-content/mu-plugins","/var/wpms-stack/internal"]:
 		ensure => directory,
 	}
 	
-	file { "/var/wpms-stack/www/wpms-stack-plugin-instructions-for-puppet":
+	file { "/var/wpms-stack/internal/wpms-stack-plugin-instructions-for-puppet":
 		ensure => present,
 		owner => "apache",
 		group => "apache",
+		require => File["/var/wpms-stack/internal"],
+	}
+
+	#delete previous file, can remove this again when everyone has updated
+	file { "/var/wpms-stack/www/wpms-stack-plugin-instructions-for-puppet":
+		ensure => absent,
 	}
 }
