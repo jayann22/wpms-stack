f95a8de27faaaf49641617383e4685f504010d08
KarelOK <karel@link7.co>
2014-05-05 12:13:01 +0800

    fix script execute permission for #4

--- setup/puppet/modules/wpms_stack/manifests/cron.pp
+++ setup/puppet/modules/wpms_stack/manifests/cron.pp
@@ -1,5 +1,10 @@
 define wpms_stack::cron() {
 	cron { "commit-to-local-git":
 		command => "/var/wpms-stack/setup/puppet/modules/wpms_stack/files/cron/commit-to-local-git.sh",
+		require => File["/var/wpms-stack/setup/puppet/modules/wpms_stack/files/cron/commit-to-local-git.sh"],
+	}
+
+	file {"/var/wpms-stack/setup/puppet/modules/wpms_stack/files/cron/commit-to-local-git.sh":
+		mode => "+x",
 	}
 }
